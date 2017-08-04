ce56b42b239bb51d59f49e9f697c99065abc7a45
tyronepost <tyrone.post.a@gmail.com>
2017-08-04 18:44:08 -0400

    updating on every keystroke working

--- app/assets/javascripts/counting_characters.coffee
+++ app/assets/javascripts/counting_characters.coffee
@@ -1,3 +1,7 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+@postCreate = (value) ->
+  url = '#{counting_characters_url}'
+  data = { form: { input: value } }
+  $.post(url, data)
--- app/controllers/counting_characters_controller.rb
+++ app/controllers/counting_characters_controller.rb
@@ -2,17 +2,17 @@ require_relative '../../app/services/repository'
 
 class CountingCharactersController < ApplicationController
   def index
-    repo = Repository.get_instance
-    @output = repo.cache
+    # repo = Repository.get_instance
+    # @output = repo.cache
   end
 
   def create
     input = params[:form][:input]
-    repo = Repository.get_instance
-    repo.cache = input
+    # repo = Repository.get_instance
+    # repo.cache = input
     @output = input
     respond_to do |format|
-      format.html { redirect_to counting_characters_url }
+      # format.html { redirect_to counting_characters_url }
       format.js
     end
   end
