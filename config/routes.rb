63a22efed8b658c557f21be7af042c0eadc78fb6
tyronepost <tyrone.post.a@gmail.com>
2017-08-08 19:04:51 -0400

    adding home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 # app/config.routes.rb
 Rails.application.routes.draw do
+  root 'home#index'
+
+  # Exercise 1: Saying Hello
   get 'say_hello/new'
   get 'say_hello/show'
   post 'say_hello' => 'say_hello#create'
 
+  # Exercise 2: Counting characters
   get 'counting_characters' => 'counting_characters#index'
   post 'counting_characters' => 'counting_characters#create'
 
+  # Exercise 3: Printing Quotes
   get 'printing_quotes' => 'printing_quotes#index'
   post 'printing_quotes' => 'printing_quotes#create'
-
   delete 'printing_quotes' => 'printing_quotes#destroy'
 end
